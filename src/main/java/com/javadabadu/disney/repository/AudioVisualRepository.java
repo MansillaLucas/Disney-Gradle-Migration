e4baf5cb3e24770dbc1490f7cca2157f44106ceb
Gabriel Pereyra <gabrielp1689@gmail.com>
2022-06-02 09:35:04 -0300

    Se elimina importacion sin uso

--- src/main/java/com/javadabadu/disney/repository/AudioVisualRepository.java
+++ src/main/java/com/javadabadu/disney/repository/AudioVisualRepository.java
@@ -1,7 +1,6 @@
 package com.javadabadu.disney.repository;
 
 import com.javadabadu.disney.models.entity.AudioVisual;
-import com.javadabadu.disney.models.entity.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.NoRepositoryBean;
